a5715a05723784746781f8087162b03a9c2bd903
Jose Garcia de Souza Junior <jose_garcia2022@outlook.com.br>
2024-09-11 11:45:30 -0300

    codigo concluido

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,71 @@
 <script setup>
+  import Formulario from './components/Formulario.vue';
+  import {reactive} from 'vue';
+
+  const estado = reactive({
+    digitoUm: '',
+    digitoDois: '',
+    troca: 'Escolher'
+  });
+
+  const getMultiplica = () => {
+    const result = estado.digitoDois * estado.digitoUm;
+    return result;
+  };
+
+  const getDivide = () => {
+    const result = estado.digitoUm / estado.digitoDois;
+    return result;
+  };
+
+  const getSubtrai = () => {
+    const result = estado.digitoUm - estado.digitoDois;
+    return result;
+  };
+
+  const getSoma = () => {
+    const result = estado.digitoUm - - estado.digitoDois;
+    return result;
+  };
+
+  const getTrocaOperacao = () => {
+    const troca = estado.troca;
+
+    switch(troca){
+      case 'Adicao':
+        return getSoma();
+      case 'Subtracao':
+        return getSubtrai();
+      case 'Multiplicacao':
+        return getMultiplica();
+      case 'Divisao':
+        return getDivide();
+      default:
+        return estado.troca;
+    };
+  };
 
 </script>
 
 <template>
-  
+  <body>
+    <header>
+      <div class="container text-bg-secondary p-5 mb-4 rounded-3">
+        <h1>Calculadora</h1>
+        <h3>VUE.js</h3>
+      </div>
+    </header>
+    <div class="container">
+      <Formulario 
+      :primeiro-digito="estado.digitoUm"
+      :segundo-digito="estado.digitoDois" 
+      :exibe-primeiro-digito="evento => estado.digitoUm = evento.target.value"
+      :exibe-segundo-digito="evento => estado.digitoDois = evento.target.value"
+      :troca-operacao="evento => estado.troca = evento.target.value"
+      :resultado="getTrocaOperacao()" 
+      />
+    </div>
+  </body>
 </template>
 
 <style scoped>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,33 @@
+<script setup>
+    const props = defineProps(['resultado', 'primeiroDigito', 'segundoDigito', 'exibePrimeiroDigito', 'exibeSegundoDigito', 'trocaOperacao']);
+</script>
+
+<template>
+    <form class="row centro">
+        <input type="number" class="form-control col espaco" :value="props.primeiroDigito" @keyup="props.exibePrimeiroDigito">
+        <select name="operacoes" class="col form-control espaco" @change="props.trocaOperacao">
+            <option value="escolher" class="text-center">Operação</option>
+            <option value="Adicao" class="text-center">( + )</option>
+            <option value="Subtracao" class="text-center">( - )</option>
+            <option value="Multiplicacao" class="text-center">( * )</option>
+            <option value="Divisao" class="text-center">( / )</option>
+        </select>
+        <input type="number" class="form-control col espaco" :value="props.segundoDigito" @keyup="props.exibeSegundoDigito">
+        <p class="col espaco_igual text-center">=</p>
+        <p class="form-control col text-center espaco">{{ props.resultado }}</p>
+    </form>
+</template>
+
+<style scoped>
+    .espaco {
+        margin: 5px;
+    }
+    .espaco_igual {
+        max-width: 5%;
+        align-content: center;
+    }
+    .centro {
+        margin: 0 auto;
+        max-width: 70%;
+    }
+</style>
